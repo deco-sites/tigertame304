c2d049b6bf98260700ce484968db2e0f6b160d5d
Ariranha <bernardo.ariranha@gmail.com>
2024-04-27 14:40:29 +0000

    Release Staging environment with 2 changes made by Ariranha

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,133 +1,62 @@
-import Image from "apps/website/components/Image.tsx";
-import type { ImageWidget } from "apps/admin/widgets.ts";
 
-
-export interface CTA {
-  id?: string;
-  href: string;
-  text: string;
-  outline?: boolean;
+interface Props {
+  searchPlaceholder?: string;
+  searchValue?: string;
 }
 
-export interface Nav {
-  navigation?: {
-    links: {
-      label?: string;
-      url?: string;
-    }[];
-    buttons: CTA[];
-  };
-}
-
-
-export const StyleType: Record<"background" | "color", string> = {
-  "background": "bg-[#020617]",
-  "color": "text-white",
-};
-
-const generateLineStyles = (position: string) => `
-  absolute ${position} z-50 block h-0.5 w-7 bg-black transition-all duration-200 ease-out 
-`;
-
-const lineStyles = [
-  generateLineStyles("top-[-0.7rem]") +
-  "peer-checked:translate-y-[0.7rem] peer-checked:rotate-[45deg]",
-  generateLineStyles("top-[-0.35rem]") + "peer-checked:opacity-0",
-  generateLineStyles("top-[0]") +
-  "peer-checked:-translate-y-[0.2rem] peer-checked:-rotate-[45deg]",
-];
-
-export default function Haader({
-  navigation = {
-    links: [
-      { label: "a revista", url: "/" },
-      { label: "notícias", url: "/" },
-      { label: "corpo editorial", url: "/" },
-      { label: "instruções aos autores", url: "/" },
-      { label: "submissões", url: "/" },
-      { label: "contato", url: "/contato" },
-    ],
-    buttons: [
-      { id: "change-me-1", href: "/", text: "en", outline: false },
-      { id: "change-me-1", href: "/", text: "pt", outline: true },
-    ],
-  },
-}: Nav) {
+export default function Header({
+  searchPlaceholder = "Search",
+  searchValue = "",
+}: Props) {
   return (
-    <nav class="container mx-auto lg:px-0 px-4 bg-[#030712]">
+    <div class="container mx-auto lg:px-0 px-4">
       <div class="flex gap-8 items-center justify-between py-4">
-        <label
-          class="cursor-pointer lg:hidden pt-6 relative z-40"
-          for="menu-mobile"
-        >
+        <div class="relative z-40 cursor-pointer lg:hidden pt-6">
           <input class="hidden peer" type="checkbox" id="menu-mobile" />
-          {lineStyles.map((style, index) => (
-            <div key={index} class={`relative ${style}`}></div>
-          ))}
-          <div class="backdrop-blur-sm bg-black/50 fixed h-full hidden inset-0 peer-checked:block w-full z-40">
-            &nbsp;
+          <div class="relative absolute top-[-0.7rem] z-50 block h-0.5 w-7 bg-black transition-all duration-200 ease-out peer-checked:translate-y-[0.7rem] peer-checked:rotate-[45deg]"></div>
+          <div class="relative absolute top-[-0.35rem] z-50 block h-0.5 w-7 bg-black transition-all duration-200 ease-out peer-checked:opacity-0"></div>
+          <div class="relative absolute top-[0] z-50 block h-0.5 w-7 bg-black transition-all duration-200 ease-out peer-checked:-translate-y-[0.2rem] peer-checked:-rotate-[45deg]"></div>
+          <div class="backdrop-blur-sm bg-black/50 fixed h-full hidden inset-0 peer-checked:block w-full z-40">&nbsp;</div>
+          <div class="fixed duration-500 overflow-y-auto overscroll-y-none peer-checked:translate-x-0 left-0 top-0 transition translate-x-full w-full z-40 h-full bg-base-100 flex flex-col float-left gap-8 min-h-full pt-12 px-6 shadow-2xl">
+            <ul class="flex flex-col gap-8">
+              <li><a href="/" aria-label="a revista">a revista</a></li>
+              <li><a href="/" aria-label="notícias">notícias</a></li>
+              <li><a href="/" aria-label="corpo editorial">corpo editorial</a></li>
+              <li><a href="/" aria-label="instruções aos autores">instruções aos autores</a></li>
+              <li><a href="/" aria-label="submissões">submissões</a></li>
+              <li><a href="/contato" aria-label="contato">contato</a></li>
+            </ul>
+            <ul class="flex items-center gap-3">
+              <a id="change-me-1" href="/" class="font-normal btn btn-primary">en</a>
+              <a id="change-me-1" href="/" class="font-normal btn btn-outline">pt</a>
+            </ul>
           </div>
-          <div class="duration-500 fixed h-full overflow-y-auto overscroll-y-none peer-checked:translate-x-0 left-0 top-0 transition translate-x-full w-full z-40">
-            <div class="bg-base-100 flex flex-col float-left gap-8 min-h-full pt-12 px-6 shadow-2xl w-1/2">
-              <ul class="flex flex-col gap-8">
-                {navigation?.links.map((link) => (
-                  <li>
-                    <a href={link.url} aria-label={link.label}>
-                      {link.label}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-              <ul class="flex items-center gap-3">
-                {navigation.buttons?.map((item) => (
-                  <a
-                    key={item?.id}
-                    id={item?.id}
-                    href={item?.href}
-                    target={item?.href.includes("http") ? "_blank" : "_self"}
-                    class={`font-normal btn btn-primary ${
-                      item.outline && "btn-outline"
-                    }`}
-                  >
-                    {item?.text}
-                  </a>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </label>
+        </div>
 
-        <ul class="hidden text-white items-center justify-between lg:flex w-full">
-          <ul class="flex">
-            {navigation.links.map((link) => (
-              <li>
-                <a
-                  href={link.url}
-                  aria-label={link.label}
-                  class="link no-underline hover:underline p-4"
-                >
-                  {link.label}
-                </a>
-              </li>
-            ))}
+        <ul class="flex items-center justify-between text-white w-full lg:flex">
+          <ul class="flex" >
+            <li><a href="/" aria-label="a revista" class="link no-underline hover:underline p-4">a revista</a></li>
+            <li><a href="/" aria-label="notícias" class="link no-underline hover:underline p-4">notícias</a></li>
+            <li><a href="/" aria-label="corpo editorial" class="link no-underline hover:underline p-4">corpo editorial</a></li>
+            <li><a href="/" aria-label="instruções aos autores" class="link no-underline hover:underline p-4">instruções aos autores</a></li>
+            <li><a href="/" aria-label="submissões" class="link no-underline hover:underline p-4">submissões</a></li>
+            <li><a href="/contato" aria-label="contato" class="link no-underline hover:underline p-4">contato</a></li>
+          </ul>
+          <ul class="relative">
+            <input type="text" placeholder="" value="" class="input placeholder-black w-full p-4 pl-12 pr-4 text-white pl-left-icon" />
+            <div class="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l5 5M10 14a4 4 0 100-8 4 4 0 000 8z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l5 5M10 14a4 4 0 100-8 4 4 0 000 8z"></path>
+              </svg>
+            </div>
           </ul>
           <ul class="flex gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item?.id}
-                id={item?.id}
-                href={item?.href}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item?.text}
-              </a>
-            ))}
+            <a href="/" class="font-normal btn btn-primary">en</a>
+            <a href="/" class="font-normal btn btn-outline">pt</a>
           </ul>
         </ul>
       </div>
-    </nav>
+    </div>
   );
 }
